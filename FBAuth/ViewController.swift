36b6eec5246b028940eed96457fa9018c5470259
William Quan <wiquan@icloud.com>
2016-05-27 04:49:32 -0700

    Official FBAuth

--- FBAuth/ViewController.swift
+++ FBAuth/ViewController.swift
@@ -7,18 +7,52 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
+import Firebase
 
-class ViewController: UIViewController {
+
+class ViewController: UIViewController, FBSDKLoginButtonDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let loginButton = FBSDKLoginButton()
+        loginButton.delegate = self
+        loginButton.readPermissions = ["public_profile", "email", "user_friends"]
+        loginButton.center = CGPointMake( self.view.bounds.size.width / 2,
+                                          3 * self.view.bounds.size.height / 4);
+        self.view.addSubview(loginButton)
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+
+    
+    func loginButton(loginButton: FBSDKLoginButton!, didCompleteWithResult result: FBSDKLoginManagerLoginResult!, error: NSError!){
+        self.performSegueWithIdentifier("next", sender: self)
+
+        if let error = error {
+            print(error.localizedDescription)
+            return
+        }
+
+        let credential = FIRFacebookAuthProvider.credentialWithAccessToken(FBSDKAccessToken.currentAccessToken().tokenString)
+        FIRAuth.auth()?.signInWithCredential(credential) { (user, error) in
+            // ...
+        }
+
+        
+    }
+    
+    func loginButtonDidLogOut(loginButton: FBSDKLoginButton!){
+        try! FIRAuth.auth()!.signOut()
+    }
+    
 
 
 }
